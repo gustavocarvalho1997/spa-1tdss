c8cf1e48549e45e5608049e2d95ff7653737b91b
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-09-15 01:44:02 -0300

    atualizando projeto

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,17 +1,16 @@
 import { Link } from "react-router-dom";
 
 export default function Cabecalho(){
-  return(
-    <>
-      {/* Crie um Cabeçalho com um h1 um título e uma lista com 3 itens. */}
-      <header>
-        <h1>Vite + React / Coded By - RM552466</h1>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/produtos">Produtos</Link></li>
-          
-        </ul>
-      </header>
-    </>
-  )
+
+    return(
+        <>
+            <header>
+                <h1>Vite + React / Coded By - RM552466</h1>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/produtos">Produtos</Link></li>
+                </ul>
+            </header>
+        </>
+    )
 }
--- src/router/EditarProdutos.jsx
+++ src/router/EditarProdutos.jsx
@@ -1,25 +1,82 @@
-import { useParams } from "react-router-dom"
-import { ListaProdutos } from "../components/ListaProdutos";
-
+import { useNavigate, useParams } from "react-router-dom";
+import { ListaProdutos } from "../Components/ListaProdutos";
+import { useState } from "react";
 
 export default function EditarProdutos() {
-  document.title = "Editar Produtos"
 
-  // O HOOK useParams()
+  document.title = `Editar Produtos` ;
+ 
+
+  //Recuperando o ID com o HOOK useParams()
   const {id} = useParams();
-//   const produtoRecuperadoById = ListaProdutos.filter((produto) =>{
-//     if(produto.id == parseInt(id)){
-//       return produto
-//     }
-// });
-const produtoRecuperadoById = ListaProdutos.filter(item => item.id == id)[0];
+
+  //Utilizando o useNavigate para realizar o redirect.
+  const navigate = useNavigate();
+
+
+  const produtoRecuperadoPorId = ListaProdutos.filter(item => item.id == id)[0];
+
+  const [produto, setProduto] = useState({
+    id: produtoRecuperadoPorId.id,
+    nome: produtoRecuperadoPorId.nome,
+    preco: produtoRecuperadoPorId.preco
+  });
+
+  const handleChangeProduto = (event)=> {
+
+        //Destructuring
+        const {name, value} = event.target;
+
+        //Popular o objeto produto com o setProduto() utilizando o operador SPREAD (...)
+        setProduto({...produto,[name]:value});
+
+  }
+
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+
+    let indice;
+    //Recuperar o indice do produto alterado com indexOf
+    indice = ListaProdutos.findIndex((item)=> item.id === produto.id);
+    
+    ListaProdutos.splice(indice,1,produto);
+    alert("Produto alterado com sucesso!");
+
+    //Realizando o redirect.
+    navigate("/produtos");
+  }
 
 
   return (
     <>
-      <h1>Editar Produtos</h1>
-      <p>Produto selecionado: {id}</p>
-      <p>Produto selecionado: {produtoRecuperadoById.nome}</p>
+      <div>
+        <h1>Editar Produtos</h1>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <fieldset>
+              <legend>Produto Selecionado</legend>
+              <div>
+                <input type="hidden" name="id"/>
+              </div>
+              <div>
+                <label htmlFor="idNome">Nome:</label>
+                <input type="text" name="nome" id="idNome" onChange={handleChangeProduto} value={produto.nome}/>
+              </div>
+              <div>
+                <label htmlFor="idPreco">Preço:</label>
+                <input type="number" name="preco" id="idPreco" onChange={handleChangeProduto} value={produto.preco}/>
+              </div>
+              <div>
+                <button>EDITAR</button>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+        <p>Nome : {produto.nome}</p>
+        <p>Preço : {produto.preco}</p>
+      </div>
     </>
+
+
   )
 }
