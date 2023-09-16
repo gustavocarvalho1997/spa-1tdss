642d5d301a097e725d5e9e71c63f804779d448fb
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-09-15 21:07:19 -0300

    db e useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
@@ -30,7 +29,7 @@ const router = createBrowserRouter([
 //Bloco de rotas
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <RouterProvider router={router}/>
-  </React.StrictMode>,
+  
 )
--- src/router/Produtos.jsx
+++ src/router/Produtos.jsx
@@ -1,18 +1,48 @@
 import { Link } from "react-router-dom"
-import { ListaProdutos } from "../components/ListaProdutos"
 import  styles from "./Produtos.module.css";
 import {AiFillEdit as Editar} from "react-icons/ai";
 import {MdDeleteForever as Excluir} from "react-icons/md";
 import { BiMessageAltAdd as Adicionar } from "react-icons/bi"
+import { useEffect, useState } from "react";
 
 export default function Produtos() {
 
   document.title = "Lista de Produtos";
 
+  const [counter, setCounter] = useState(0);
+
+  const [counter2, setCounter2] = useState(0)
+
+  useEffect(() => {
+    console.log("useEffect será renderizado sempre que o componente ou qualquer objeto for atualizado!");
+  });
+  useEffect(() => {
+    console.log("useEffect será renderizado apenas uma vez!");
+  },[]);
+
+  const [produtos, setProdutos] = useState([{}])
+
+  useEffect(() => {
+    console.log("useEffect será renderizado apenas se um objeto/variável/constante que estiver no array de dependências sofre uma atualização!");
+    fetch("http://localhost:5000/produtos")
+    .then((lista) => lista.json())
+    .then((listProdutos) => {
+      setProdutos(listProdutos)
+    })
+  },[counter2]);
+  
+
 
   return (
     <div>
         <h1>Produtos</h1>
+
+        <div>
+          <button onClick={() => setCounter(counter + 1)}>COUNTER - {counter}</button>
+        </div>
+        <div>
+          <button onClick={() => setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
+        </div>
 
         <table className={styles.table}>
             <thead>
@@ -24,7 +54,7 @@ export default function Produtos() {
             </tr>
             </thead>
             <tbody>
-            {ListaProdutos.map((produto,indice)=>(
+            {produtos.map((produto,indice)=>(
                  <tr key={indice} className={styles.tableRow}>
                     <td className={styles.tableData}>{produto.id}</td>
                     <td className={styles.tableData}>{produto.nome}</td>
