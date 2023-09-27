031014f49e81df6c50da3934b574048f5e1264ca
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-09-27 18:45:07 -0300

    ajuste

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,32 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-//Bloco de rotas
+
+//BLOCO DE ROTAS
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './router/Home.jsx';
-import Produtos from './router/Produtos.jsx';
-import EditarProdutos from './router/EditarProdutos.jsx';
-import Erro404 from './router/Erro404.jsx';
-import ExcluirProdutos from './router/ExcluirProdutos.jsx';
-import AdicionarProdutos from './router/AdicionarProdutos.jsx';
+import EditarProdutos from './routes/EditarProdutos.jsx';
+import Erro404 from './routes/Erro404.jsx';
+import Home from './routes/Home.jsx';
+import Produtos from './routes/Produtos.jsx';
+import ExcluirProdutos from './routes/ExcluirProdutos.jsx';
 
 //Criando o objeto de rotas
 const router = createBrowserRouter([
   { path: '/', element: <App />,
-    errorElement: <Erro404 />,
-    children: [
-      { path: '/', element: <Home /> },
-      { path: '/produtos', element: <Produtos /> },
-      { path: '/editar/produtos/:id', element: <EditarProdutos /> },
-      { path: '/excluir/produtos/:id', element: <ExcluirProdutos /> },
-      { path: '/adicionar/produtos', element: <AdicionarProdutos /> },
-    ]
+      errorElement: <Erro404 />, 
+      children: [
+        { path: '/', element: <Home /> },
+        { path: '/produtos', element: <Produtos /> },
+        { path: '/editar/produtos/:id', element: <EditarProdutos /> },
+        { path: '/excluir/produtos/:id', element: <ExcluirProdutos /> },
+      ]
   },
 ])
 
-
-
-//Bloco de rotas
+//BLOCO DE ROTAS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
     <RouterProvider router={router}/>
-  
+  ,
 )
--- src/router/Produtos.jsx
+++ src/router/Produtos.jsx
@@ -9,40 +9,32 @@ export default function Produtos() {
 
   document.title = "Lista de Produtos";
 
-  const [counter, setCounter] = useState(0);
-
-  const [counter2, setCounter2] = useState(0)
+  const [produtos, setProdutos] = useState([{}]);
 
   useEffect(() => {
-    console.log("useEffect será renderizado sempre que o componente ou qualquer objeto for atualizado!");
-  });
-  useEffect(() => {
-    console.log("useEffect será renderizado apenas uma vez!");
+    console.log("useEffect será rendereizado apenas uma vez!");
+    fetch("http://localhost:5000/produtos",{
+      method: "GET",
+      headers:{
+        "Content-Type": "application/json"
+      }
+      })
+      .then((response)=> response.json())
+      .then((listaProdutos)=>{
+          setProdutos(listaProdutos);
+      })
+  
   },[]);
 
-  const [produtos, setProdutos] = useState([{}])
-
-  useEffect(() => {
-    console.log("useEffect será renderizado apenas se um objeto/variável/constante que estiver no array de dependências sofre uma atualização!");
-    fetch("http://localhost:5000/produtos")
-    .then((lista) => lista.json())
-    .then((listProdutos) => {
-      setProdutos(listProdutos)
-    })
-  },[counter2]);
-  
+  const [open, setOpen] = useState(false);
 
 
   return (
     <div>
         <h1>Produtos</h1>
 
-        <div>
-          <button onClick={() => setCounter(counter + 1)}>COUNTER - {counter}</button>
-        </div>
-        <div>
-          <button onClick={() => setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
-        </div>
+        {open ? <ModalInserir open={open} setOpen={setOpen} /> : "" }
+        <button onClick={()=> setOpen(true)}>OPEN - MODAL</button>
 
         <table className={styles.table}>
             <thead>
